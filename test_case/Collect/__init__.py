4b51551e1c07becc14c93da3a35f226f5c1564ec
张猛 <kailzhang893@outlook.com>
2023-06-06 22:36:54 +0800

    Initial commit

---
+++ test_case/Collect/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+-----------------------------------------------------------
+    @FileName  :__init__.py.py
+    @Time      :2023/6/6 22:29
+    @Author    :LittleSeven
+    @Address   ：https://gitee.com/linguchong/Django_Project_Demo.git
+-----------------------------------------------------------
+'''
